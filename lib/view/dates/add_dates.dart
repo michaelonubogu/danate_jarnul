d0f2aacd7e9270ee9df718cb1c78750c6a3eeaa8
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-04-14 07:04:10 +0600

    ambers profiles added

--- lib/view/dates/add_dates.dart
+++ lib/view/dates/add_dates.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'view_controller/show_admirer_popup.dart';
@@ -18,6 +19,10 @@ class _AddDatesState extends State<AddDates> {
   final time = TextEditingController();
   final location = TextEditingController();
   final addDateFormKey = GlobalKey<FormState>();
+
+  var items = ["a", "b"];
+
+  var selectedValue;
 
   @override
   Widget build(BuildContext context) {
@@ -85,18 +90,48 @@ class _AddDatesState extends State<AddDates> {
                 ),
               ),
               SizedBox(height: 15,),
-              TextFormField(
-                readOnly: true,
-                onTap: ()=>ShowAdmirerPopup.showAdmirerPopup(size, context),
-                controller: title,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: "Choose Sign",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none
-                    )
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: AppColors.blue, width: 1)
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    hint: Text(
+                      'Select Item',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                    items: []
+                        .map((item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ))
+                        .toList(),
+                    value: selectedValue,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedValue = value as String;
+
+                      });
+                    },
+                    buttonStyleData: const ButtonStyleData(
+                      height: 40,
+                      width: 140,
+                    ),
+                    menuItemStyleData: const MenuItemStyleData(
+                      height: 40,
+                    ),
+                  ),
                 ),
               ),
               SizedBox(height: 30,),
