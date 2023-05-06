d7d8103e68020300f9b46a97d50d491c50d68784
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-05-07 00:24:14 +0600

    crete boxes for Journal

--- lib/boxs/boxs.dart
+++ lib/boxs/boxs.dart
@@ -1,5 +1,6 @@
 
 import 'package:dante/model/auth_model/email_verify_model.dart';
+import 'package:dante/model/journal_model/journal_model.dart';
 import 'package:hive/hive.dart';
 import '../model/admirers_model/admirers_model.dart';
 import '../model/dates_model/dates_screen_model.dart';
@@ -12,5 +13,6 @@ class Boxes{
   static Box<AdmirerModel> getAdmirers = Hive.box<AdmirerModel>("admirers");
   static Box<ProfileModel> getProfile = Hive.box<ProfileModel>("profiles");
   static Box<DatesModel> getDates = Hive.box<DatesModel>("dates");
+  static Box<JournalModel> getJournal = Hive.box<JournalModel>("journals");
 
 }
