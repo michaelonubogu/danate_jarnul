277216200f1b742728ab7244bdc0fc39757dd882
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-05-23 17:25:31 +0600

    search by time and title finish

--- lib/view/journals/Journals.dart
+++ lib/view/journals/Journals.dart
@@ -2,6 +2,7 @@ import 'package:dante/boxs/boxs.dart';
 import 'package:dante/model/journal_model/journal_model.dart';
 import 'package:dante/utility/app_colors.dart';
 import 'package:dante/view/journals/single_journal.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
@@ -17,6 +18,14 @@ class Journals extends StatefulWidget {
 class _JournalsState extends State<Journals> {
   //search controller
   final search = TextEditingController();
+  final searchDate = TextEditingController();
+
+  List<String> filterText = [
+    "Shop By Time",
+    "Shop By Name"
+  ];
+
+  bool isSearchByTime = false;
 
 
   @override
@@ -37,7 +46,10 @@ class _JournalsState extends State<Journals> {
       var data = Boxes.getJournal.getAt(i)!;
       print("journals === ${data!.title}");
       print("journals === ${data!.details}");
-      journalList.add(data);
+      if(data!.token == userToken){
+        journalList.add(data);
+      }
+
     }
     setState(() {
 
@@ -99,33 +111,91 @@ class _JournalsState extends State<Journals> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(
-                  width: size.width*.70,
-                  child: TextFormField(
-                    controller: search,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: "Search notes",
-                      prefixIcon: Icon(Icons.search_outlined, size: 40,),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
-                      )
+               isSearchByTime
+                   ?  SizedBox(
+                 width: size.width*.70,
+                 child: TextFormField(
+                   controller: searchDate,
+                   onTap: ()=>selectDate(),
+                   readOnly: true,
+                   decoration: InputDecoration(
+                       filled: true,
+                       fillColor: Colors.white,
+                       hintText: "Search by Date",
+                       prefixIcon: Icon(Icons.search_outlined, size: 40,),
+                       suffixIcon: Icon(Icons.calendar_month_outlined, size: 25,),
+                       border: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(10),
+                         borderSide: BorderSide.none,
+                       )
+                   ),
+                 ),
+               )
+                   : SizedBox(
+                 width: size.width*.70,
+                 child: TextFormField(
+                   controller: search,
+                   onChanged: (value){
+                     searchJournal(value);
+                   },
+                   decoration: InputDecoration(
+                       filled: true,
+                       fillColor: Colors.white,
+                       hintText: "Search notes",
+                       prefixIcon: Icon(Icons.search_outlined, size: 40,),
+                       border: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(10),
+                         borderSide: BorderSide.none,
+                       )
+                   ),
+                 ),
+               ),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    dropdownStyleData: DropdownStyleData(
+                      width: 160,
+                      padding: const EdgeInsets.symmetric(vertical: 6,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      elevation: 2,
+                      offset: const Offset(0, 8),
                     ),
+                   customButton: Container(
+                     width: 58,
+                     height: 58,
+                     decoration: BoxDecoration(
+                       color: AppColors.mainColor.withOpacity(0.8),
+                       borderRadius: BorderRadius.circular(10),
+                     ),
+                     child: Center(
+                       child: Image.asset("assets/images/filter.png", height: 40, width: 40,),
+                     ),
+                   ),
+                    items: filterText
+                        .map((item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ))
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        if(value == "Shop By Time"){
+                          isSearchByTime = true;
+                        }else{
+                          isSearchByTime = false;
+                        }
+                      });
+                    },
+
                   ),
                 ),
-                Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    color: AppColors.mainColor.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Image.asset("assets/images/filter.png", height: 40, width: 40,),
-                  ),
-                )
+
               ],
             ),
 
@@ -139,14 +209,15 @@ class _JournalsState extends State<Journals> {
             ),
             SizedBox(height: 20,),
 
-            Expanded(
-              child: ListView.builder(
+            isSearch
+                ?searchList.isNotEmpty ? Expanded(
+                  child: ListView.builder(
                 shrinkWrap: true,
-                itemCount: journalList.length,
+                itemCount: searchList.length,
                 itemBuilder: (context, index){
-                  var data = journalList[index]!;
+                  var data = searchList[index]!;
 
-               
+
                   return InkWell(
                     onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>SingleJournal(journalModel: data,))),
                     child: Container(
@@ -162,9 +233,9 @@ class _JournalsState extends State<Journals> {
                         children: [
                           Text("${data.title}",
                             style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black
                             ),
                           ),
                           SizedBox(height: 20,),
@@ -176,8 +247,79 @@ class _JournalsState extends State<Journals> {
                                 child: Row(
                                   children: [
                                     ClipRRect(
-                                        borderRadius: BorderRadius.circular(100),
-                                        child: Image.memory(data.admirers["profile"], height: 40, width: 40, fit: BoxFit.cover,),
+                                      borderRadius: BorderRadius.circular(100),
+                                      child: Image.memory(data.admirers["profile"], height: 40, width: 40, fit: BoxFit.cover,),
+                                    ),
+                                    SizedBox(width: 10,),
+                                    Text("${data.admirers["name"]}",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                          color: AppColors.textColor
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(DateFormat.yMMMd().format(DateTime.parse("${data.dateTime}")),
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.textColor
+                                ),
+                              ),
+
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+                )
+                :Center(child: Text("No journal found!"),)
+
+                : isDateSearchList
+                ?searchJournalWithDateList.isNotEmpty ? Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: searchJournalWithDateList.length,
+                itemBuilder: (context, index){
+                  var data = searchJournalWithDateList[index]!;
+
+
+                  return InkWell(
+                    onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>SingleJournal(journalModel: data,))),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.only(bottom: 20),
+                      decoration: BoxDecoration(
+                        color: Color(data.color),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("${data.title}",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: size.width*.40,
+                                child: Row(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(100),
+                                      child: Image.memory(data.admirers["profile"], height: 40, width: 40, fit: BoxFit.cover,),
                                     ),
                                     SizedBox(width: 10,),
                                     Text("${data.admirers["name"]}",
@@ -207,6 +349,76 @@ class _JournalsState extends State<Journals> {
                 },
               ),
             )
+                :Center(child: Text("No journal found!"),)
+
+                : journalList.isNotEmpty ? Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: journalList.length,
+                    itemBuilder: (context, index){
+                      var data = journalList[index]!;
+
+
+                      return InkWell(
+                        onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=>SingleJournal(journalModel: data,))),
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          margin: EdgeInsets.only(bottom: 20),
+                          decoration: BoxDecoration(
+                            color: Color(data.color),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("${data.title}",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black
+                                ),
+                              ),
+                              SizedBox(height: 20,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  SizedBox(
+                                    width: size.width*.40,
+                                    child: Row(
+                                      children: [
+                                        ClipRRect(
+                                            borderRadius: BorderRadius.circular(100),
+                                            child: Image.memory(data.admirers["profile"], height: 40, width: 40, fit: BoxFit.cover,),
+                                        ),
+                                        SizedBox(width: 10,),
+                                        Text("${data.admirers["name"]}",
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w600,
+                                              color: AppColors.textColor
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Text(DateFormat.yMMMd().format(DateTime.parse("${data.dateTime}")),
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: AppColors.textColor
+                                    ),
+                                  ),
+
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ):Center(child: Text("No journal yet!"),)
 
 
 
@@ -230,4 +442,81 @@ class _JournalsState extends State<Journals> {
       ),
     );
   }
+
+  //initial current date & time appear
+  List<JournalModel> searchList = [];
+
+  bool isSearch = false;
+
+  void searchJournal(String value) {
+    print("this is search text === $value");
+    if(value.isNotEmpty){
+      searchList.clear();
+      setState(() {
+        isSearch =true;
+      });
+      if(journalList.isNotEmpty){
+        for(var i in journalList){
+          if(i.title.contains(value)){
+            searchList.add(i);
+          }
+        }
+      }
+
+    }else{
+      setState(() {
+        isSearch =false;
+      });
+    }
+
+    print("this is seqarch === ${isSearch}");
+  }
+
+
+  //Show journal with date & time appear
+  List<JournalModel> searchJournalWithDateList = [];
+  bool isDateSearchList = false;
+  void searchJournalWithDate(String date) {
+    print("this is search text === $date");
+    if(date.isNotEmpty){
+      searchJournalWithDateList.clear();
+      setState(() {
+        isDateSearchList =true;
+      });
+      if(journalList.isNotEmpty){
+        for(var i in journalList){
+          if(DateFormat.yMMMd().format(DateTime.parse("${i.dateTime}")).contains(date)){
+            searchJournalWithDateList.add(i);
+          }
+        }
+      }
+
+    }else{
+      setState(() {
+        isDateSearchList =false;
+      });
+    }
+
+    print("this is seqarch === ${isSearch}");
+  }
+
+
+
+
+  var selectedDate;
+  //select date
+  Future selectDate() async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1920, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = DateFormat.yMMMd().format(picked);
+        searchDate.text = selectedDate;
+        searchJournalWithDate(selectedDate);
+      });
+  }
+
 }
